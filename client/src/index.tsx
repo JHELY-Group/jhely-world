063e4ef854a6d570bda86d26d3f656b4b39f81ae
Jeffrey <jeffreyhu16@gmail.com>
2022-11-08 08:56:19 +0800

    umcomment strict mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,11 +9,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <RoomProvider>
       <App />
     </RoomProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
